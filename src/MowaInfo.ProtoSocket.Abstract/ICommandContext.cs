cd7452e2c8fc91ffebfbb8ca774fc915d2d7b7e1
Sjhzj <543956585@qq.com>
2017-09-10 23:07:52 +0800

    Add ICommandContext.

---
+++ src/MowaInfo.ProtoSocket.Abstract/ICommandContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+
+namespace MowaInfo.ProtoSocket.Abstract
+{
+    public interface ICommandContext
+    {
+        IMessageContainer Request { get; set; }
+        IMessageContainer Response { get; set; }
+        IChannel Channel { get; set; }
+        IDictionary<object, object> Items { get; set; }
+        IServiceProvider RequestServices { get; set; }
+        CancellationToken RequestAborted { get; set; }
+        void Abort();
+    }
+}
